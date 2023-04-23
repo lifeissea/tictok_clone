e92428ce3eaa22778a96ebe0727a0df55336dbce
lifeissea <lifeissea@naver.com>
2023-04-23 18:59:02 +0900

    2.8 까지 완료

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -5,6 +5,12 @@ class LoginFormScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Log in",
+        ),
+      ),
+    );
   }
 }
